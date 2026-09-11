e0dd331a0b30f176e7c2c8714f0b6196fdb2bd01
ImpulsoWeb10 <rafagds130303@gmail.com>
2026-09-10 22:52:24 -0300

    Create produtos.js

---
+++ AtendimentoaoclienteQualy/produtos.js
@@ -0,0 +1,55 @@
+[
+    {
+        "id": 1789091184532,
+        "nome": "Tinta Econômica Qualy Color",
+        "categoria": "Tinta",
+        "descricao": "Tinta Acrílica Qualy Color Projetada para uso interno paredes novas ou encima de cores Claras.",
+        "uso": "Interno",
+        "superficies": "Paredes internas de casas e apartamentos, paredes com reboco novo, e pós seladora ou massa.",
+        "superficiesNao": "Áreas externas ou fachadas, pois o produto não resiste bem à chuva, sol e intempéries.Áreas úmidas ou molhadas, como banheiros, cozinhas e lavanderias (locais que exigem tintas do tipo standard ou premium com maior resistência à lavabilidade e umidade)",
+        "indicacoes": "Superfícies de reboco, massa corrida, massa acrílica, concreto e fibrocimento em locais protegidos da umidade.",
+        "rendimento": "até 150m2",
+        "acabamento": "Fosco",
+        "cores": "Branco e cores claras ( Consultar )",
+        "diluicao": "Já vem pronta pra uso, mexer bem com misturador, estando bem homogenia , pode aplicar.",
+        "aplicacao": "O modo de aplicação da tinta acrílica económica segue um passo a passo simples, mas que exige atenção à preparação e aos intervalos para garantir um bom acabamento.1. Preparação da SuperfícieLimpeza: A parede deve estar completamente limpa, seca e livre de pó, gordura, brilho ou mofo.Lixamento: Lixe a superfície para remover imperfeições e limpe o pó com um pano húmido antes de começar.2. Misture muito bem (homogeneíze). AplicaçãoRecortes: Comece a pintura fazendo os cantos e rodapés com uma trincha (pincel).Pintura geral: Utilize um rolo de lã de pelo baixo (indicado para tintas acrílicas). Aplique a tinta na parede fazendo movimentos em forma de \"W\" ou \"M\" para distribuir o produto uniformemente, e depois preencha os espaços.Número de demãos: São necessárias de 2 a 3 demãos para obter uma cobertura uniforme.4. Intervalos e SecagemEntre demãos: Aguarde de 2 a 4 horas antes de aplicar a demão seguinte.Secagem total: O processo de secagem completa demora cerca de 12 horas.",
+        "secagem": "4 por demão",
+        "caracteristicas": "Antimofo,econômica,facil aplicar, facil limpar, baixo odor.",
+        "observacoes": "Aqui estão as observações importantes e os cuidados essenciais que deve ter ao trabalhar com a tinta acrílica económica:⚠️ Limitações do ProdutoBaixa resistência à lavagem: A tinta económica não aguenta esfregar com esponjas ou produtos de limpeza. Para limpar a parede depois de pronta, use apenas um pano macio levemente humedecido com água, sem fazer força.Cobertura inferior: Por ter menos pigmento que as tintas Standard e Premium, ela pode exigir mais demãos se estiver a tentar cobrir uma cor escura ou um reboco novo.",
+        "variacoes": [
+            {
+                "embalagem": "3,6 Litros",
+                "preco": "32.9",
+                "rendimento": "30m2",
+                "estoque": "100",
+                "sku": "Qc_3.6_br"
+            },
+            {
+                "embalagem": "Balde 18 Litros",
+                "preco": "99.9",
+                "rendimento": "150m2",
+                "estoque": "100",
+                "sku": "Qc_Bld_18lts"
+            },
+            {
+                "embalagem": "Lata 18 Litros",
+                "preco": "110.9",
+                "rendimento": "30m2",
+                "estoque": "100",
+                "sku": "Qc_Lt_18lts"
+            }
+        ],
+        "perguntas": [
+            {
+                "pergunta": "Quando rende essa econômica ?",
+                "resposta": "De 18lts até 150m2"
+            },
+            {
+                "pergunta": "Pode usar do lado de fora de casa ?",
+                "resposta": "Não, produto interno."
+            }
+        ],
+        "linkCompra": "",
+        "linkWhatsApp": ""
+    }
+]
