941a92f3c64f6a70e2a04ebff24f513d711d55be
ImpulsoWeb10 <rafagds130303@gmail.com>
2026-09-14 21:54:09 -0300

    Update APPS_SCRIPT_URL to the correct endpoint

--- appmotos/js/app.js
+++ appmotos/js/app.js
@@ -1,5 +1,5 @@
 
-const APPS_SCRIPT_URL = "SUA_URL_DO_APPS_SCRIPT_AQUI";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwdA_m_8TkJ64LHnwizwBWBhfdBFcHquE4d2TNirlKMb8w00LOMIbDQAf7y26RmzKVu/exec";
 
 let state = {
   clientes: [],
