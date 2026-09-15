e540156a2ebc31e2371d3a4cfe18a908c298b30e
ImpulsoWeb10 <rafagds130303@gmail.com>
2026-09-14 21:51:24 -0300

    Add service worker for caching and fetch handling

--- appmotos/sw.js
+++ appmotos/sw.js
@@ -1,1 +1,8 @@
 
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('oficina-v1').then((cache) => cache.addAll(['./', './index.html', './css/styles.css', './js/app.js'])));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
